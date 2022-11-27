d11e3ef7b3cf74cb8291640b740b3906f1b538a6
arifusinbad <ariful.islam.sinbad@gmail.com>
2022-11-27 17:08:22 +0600

    commit 7

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 require('dotenv').config()
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+
 const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
@@ -40,6 +42,7 @@ const productsCollection = client.db('booksMarket').collection('products')
 const userInfoCollection = client.db('booksMarket').collection('UserInfo')
 const addProductCollection = client.db('booksMarket').collection('addProduct')
 const usersCollection = client.db('booksMarket').collection('users')
+const paymentsCollection = client.db('booksMarket').collection('payments')
 app.get('/products', async (req, res)=>{
   const query = {}
   const products = await productsCollection.find(query).toArray()
@@ -55,6 +58,38 @@ app.get('/productSpacialty', async(req, res)=>{
   const query = {}
   const result = await productsCollection.find(query).project({name: 1}).toArray()
   res.send(result)
+})
+
+app.post("/create-payment-intent", async(req, res)=>{
+  const userInfo = req.body;
+  const price = parseInt(userInfo.price);
+  const amount = price * 100;
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount,
+    currency: 'usd',
+    "payment_method_types": [
+      "card"
+    ],
+  });
+  
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+})
+app.post('/payments', async (req, res)=>{
+  const payment = req.body;
+  const payments = await paymentsCollection.insertOne(payment)
+const id = payment.bookingId
+const filter = {_id: ObjectId(id)}
+const updateDoc = {
+  $set:{
+    paid: true,
+    transactionId: payment.transactionId
+  }
+}
+const result = await userInfoCollection.updateOne(filter, updateDoc)
+
+  res.send(payments)
 })
 
 app.post('/users', async(req, res)=>{
